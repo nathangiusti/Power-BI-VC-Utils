845d42a0c1a40d43cfa77cdc8e2569c92b3fdc70
Nathan Giusti <bigbadbyte2@gmail.com>
2021-10-24 11:30:09 -0400

    Action now prints to one file for each report section

--- scripts/run.py
+++ scripts/run.py
@@ -3,23 +3,49 @@ import json
 import sys
 import os.path
 
+# The characters are illegal in file names and will be replaced with _
+ILLEGAL_PATH_CHARACTERS = {'<', '>', ':', '/', '\\', '|', '?', '*', ' '}
+
 for file in sys.argv:
+    # For JSON files, just read and pretty print
     if file.endswith('.json') and os.path.exists(file):
         with open(file, 'r', encoding='utf-8-sig') as f:
             json_str = json.dumps(json.load(f), indent=4)
         with open(file, 'w') as f:
             f.write(json_str)
         print('Pretty Printed {}'.format(file))
+
     if file.endswith('.pbix') and os.path.exists(file):
+        # Get the PBIX file name to use as the directory name
+        pbix_file_name = file[:-5]
+
+        # Create the directory if it doesn't exist. Remove all existing files from directory if it does
+        os.makedirs(pbix_file_name, exist_ok=True)
+        for f in os.listdir(pbix_file_name):
+            os.remove(os.path.join(pbix_file_name, f))
+
+        # PBIX is just a zip with utf-16-le encoding
         zf = zipfile.ZipFile(file)
         data = json.loads(zf.read('Report/Layout').decode('utf-16-le'))
         data['config'] = json.loads(data['config'])
-        for section in data['sections']:
+        if 'filters' in data:
+            data['filters'] = json.loads(data['filters'])
+
+        # Remove and process sections separately from the rest of the document
+        sections = data.pop('sections')
+        for section in sections:
             for visual_container in section['visualContainers']:
                 for key in ['config', 'filters', 'query', 'dataTransforms']:
                     if key in visual_container.keys():
                         visual_container[key] = json.loads(visual_container[key])
-        output_path = file[:-5] + '.json'
+            section_name = section['displayName'].translate({ord(x): '_' for x in ILLEGAL_PATH_CHARACTERS})
+            output_path = pbix_file_name + '\\' + section_name + '.json'
+            with open(output_path, "w") as f:
+                json.dump(section, f, indent=4)
+
+        # Dump rest of the PBIX JSON
+        output_path = pbix_file_name + '\\' + pbix_file_name + '.json'
         with open(output_path, "w") as f:
             json.dump(data, f, indent=4)
+
         print('Pretty Printed {}'.format(file))
